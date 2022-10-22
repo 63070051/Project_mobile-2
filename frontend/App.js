88344907ce52e49a78393c292b1cee305635541c
O <wongsapatasavawongsanon@Os-Macbook.local>
2022-10-22 19:01:25 +0700

    test

--- frontend/App.js
+++ frontend/App.js
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./app/screen/Home";
 import ManageRole from "./app/screen/ManageRole";
+import Course from "./app/screen/Course";
 export default function App() {
   return (
-    <ManageRole />
+    <Course/>
   );
 }
 
