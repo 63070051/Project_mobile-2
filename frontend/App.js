f6cc01dfec35060c9683f1735459f0ab6db5e8dd
mindeiei <73679335+TanavichL@users.noreply.github.com>
2022-10-23 16:42:21 +0700

    merge

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./app/screen/home";
-<<<<<<< HEAD
 import Register from "./app/screen/register";
+import Login from "./app/screen/login";
+
 export default function App() {
   return (
     <Register />
-=======
-import Login from "./app/screen/login";
-export default function App() {
-  return (
-    <Login />
->>>>>>> 6e57cb8d80e3b3c52000f7a654913cc2919fb703
   );
 }
 
