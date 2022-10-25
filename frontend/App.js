dade039247ce237575cf3ee4b9cc9a227b8278eb
O <wongsapatasavawongsanon@Os-Macbook.local>
2022-10-25 15:23:07 +0700

    course

--- frontend/App.js
+++ frontend/App.js
@@ -3,19 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 import Home from "./app/screen/home";
 import Login from "./app/screen/login";
 import ManageRole from "./app/screen/ManageRole";
-<<<<<<< HEAD
 import Course from "./app/screen/Course";
-import navbar from "./app/component/navbar";
-export default function App() {
-  return (
-    <ManageRole/>
-=======
 import Register from "./app/screen/register";
 import MyNavigator from "./app/navigator/navigation";
 export default function App() {
   return (
-    <MyNavigator />
->>>>>>> feb6b6edd2a536bf0e9ad7180acbd263d7314e41
+    <MyNavigator/>
   );
 }
 
