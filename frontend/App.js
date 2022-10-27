032092ab283e002788f06d198c4715c055fcdb5b
mindeiei <73679335+TanavichL@users.noreply.github.com>
2022-10-27 14:31:30 +0700

    merge

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./app/screen/home";
-import Register from "./app/screen/register";
 import Login from "./app/screen/login";
 import ManageRole from "./app/screen/ManageRole";
 import Course from "./app/screen/Course";
